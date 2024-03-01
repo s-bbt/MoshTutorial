7b8f7eba3cb95c9c282c794b6e1860bf4baad85d
Sovann <skb@ik.me>
2024-03-01 11:20:50 +0100

    Base projet React

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Message from './Message.tsx';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+      <div><NavBar/><Message/></div>
+  )
+}
+
+export default App
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+function NavBar() {
+    return (
+        <nav className={"nav navbar-expend-lg g-body-tertiary"}>
+            <div className={"container-fluid"}>
+                <a className={"navbar-brand"} href={"#"}>Menu</a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
